5c983c2ad021e8b6b0428f67a7a1172005a546ca
tanujvyas10 <48439116+tanujvyas10@users.noreply.github.com>
2021-02-28 23:20:57 +0530

    Update server.js

--- chatapp-backend/server.js
+++ chatapp-backend/server.js
@@ -25,16 +25,6 @@ const users=require("./routes/userRoute")
 const friends=require("./routes/friendsRoute")
 const message=require("./routes/messageRoute")
 
-// app.use((req,res,next)=>{
-//   res.header("Access-Control-Allow-Origin","*");
-//   res.header("Access-Control-Allow-Credentials","true");
-//   res.header("Access-Control-Allow-Methods",'GET','POST','DELETE','PUT','OPTION');4
-//   res.header(
-//       'Access-Control-Allow-Headers',
-//       'Origin,X-Requested-With,Content-Type,Accept,Authorization'
-//   );
-//   next();
-// })
 
 app.use(express.json({limit:'50mb'}))
 app.use(express.urlencoded({extended:true,limit:'50mb'}))
